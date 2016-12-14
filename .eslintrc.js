e345dc5670b343ef8dd1800aa84c096633a6f879
Teddy Katz <teddy.katz@gmail.com>
2016-12-13 21:06:43 -0500

    Build: Dogfood this plugin's rules on its own codebase

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const PACKAGE_NAME = require('./package').name;
+const SYMLINK_LOCATION = path.join(__dirname, 'node_modules', PACKAGE_NAME);
+
+// Symlink node_modules/{package name} to this directory so that ESLint resolves this plugin name correctly.
+if (!fs.existsSync(SYMLINK_LOCATION)) {
+  fs.symlinkSync(__dirname, SYMLINK_LOCATION);
+}
+
+module.exports = {
+  plugins: [
+    'node',
+    PACKAGE_NAME,
+  ],
+  extends: [
+    'not-an-aardvark/node',
+    'plugin:node/recommended',
+    'plugin:eslint-plugin/recommended',
+  ],
+  root: true,
+  rules: {
+    'require-jsdoc': 'error',
+  },
+};
