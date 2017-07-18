93bd142cbf6159561099ad5e3cdfd8f1f90503f5
薛定谔的猫 <hh_2013@foxmail.com>
2017-07-18 09:48:56 +0800

    New: fixer-return (#15)
    * New: fixer-return
    
    * Update: support old-style context.report calls.
    
    * Update: check context.report({fix})
    
    * Docs: fixer-return.md
    
    * Docs: fixer-return in readme.md
    
    * Fix: accept review suggestions.

---
+++ tests/lib/rules/fixer-return.js
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview enforces always return from a fixer function
+ * @author 薛定谔的猫<hh_2013@foxmail.com>
+ */
+
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/fixer-return');
+const RuleTester = require('eslint').RuleTester;
+
+const ERROR = { message: 'Expected fixer function to always return a value.' };
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+ruleTester.run('fixer-return', rule, {
+  valid: [
+    `
+    module.exports = {
+        create: function(context) {
+            context.report( {
+                fix: function(fixer) {
+                    return fixer.foo();
+                }
+            });
+        }
+    };
+    `,
+    `
+    module.exports = {
+        create: function(context) {
+            context.report({
+                fix: fixer => fixer.foo()
+            });
+        }
+    };
+    `,
+  ],
+
+  invalid: [
+    {
+      code: `
+      module.exports = {
+          create: function(context) {
+              context.report({
+                  fix(fixer) {
+                      fixer.foo();
+                  }
+              });
+          }
+      };
+      `,
+      errors: [ERROR],
+    },
+  ],
+});
