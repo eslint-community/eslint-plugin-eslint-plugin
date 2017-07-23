6137274a28a5b3036bc41933d96222ab6ed1c6a0
薛定谔的猫 <hh_2013@foxmail.com>
2017-07-23 20:13:27 +0800

    New: rule test-case-property-ordering (#16)
    * New: rule test-case-property-ordering
    
    * Update: schema
    
    * Update: use utils.getKeyName
    
    * Update: rename j to lastChecked
    
    * Chore: tests to be ordered.
    
    * Chore: add test for option.
    
    * docs: test-case-property-ordering.md
    
    * Docs: rule test-case-property-order in readme.md
    
    * Docs: rule test-case-property-order (+1 squashed commits)
    
    Squashed commits:
    
    [67b777a] Docs: rule test-case-property-order
    
    * Update: change the error message to only contain properties that appear in the test case.
    
    * Update: change the error message to only contain properties that appear in the test case.
    
    * docs
    
    * Chore: correct variable name in comment.
    
    * Fix: ACCept review suggestions.
    
    * Update: extra properties error message.

---
+++ docs/rules/test-case-property-ordering.md
@@ -0,0 +1,56 @@
+# enforce ordering of keys in test cases (test-case-property-ordering)
+
+This rule enforces that the properties of RuleTester test cases are arranged in a consistent order.
+
+## Rule Details
+
+### Options
+
+This rule has an array option:
+
+* `["code", "output", "options", "parserOptions", "errors"]` (default): The properties of a test case should be placed in a consistent order.
+
+Examples of **incorrect** code for this rule:
+
+```js
+
+/* eslint eslint-plugin/test-case-property-ordering: ["error",
+  ["code", "output", "options", "parserOptions", "errors"]
+] */
+
+// invalid; wrong order
+{
+  code: "foo",
+  options: ["baz"],
+  output: "bar",
+}
+
+
+// invalid; extra properties should need to be placed afterwards.
+{
+  code: "foo",
+  env: { es6: true }
+  output: "bar",
+  options: ["baz"],
+}
+```
+
+Examples of **correct** code for this rule:
+
+```js
+/* eslint eslint-plugin/test-case-property-ordering: ["error",
+  ["code", "output", "options", "parserOptions", "errors"]
+] */
+
+// valid;
+{
+  code: "foo",
+  output: "bar",
+  options: ["baz"],
+}
+
+```
+
+## When Not To Use It
+
+If don't want to enforce ordering of keys in test cases, you can turn off this rule.
---
+++ tests/lib/rules/test-case-property-ordering.js
@@ -0,0 +1,139 @@
+/**
+ * @fileoverview Requires the properties of a test case to be placed in a consistent order.
+ * @author 薛定谔的猫<hh_2013@foxmail.com>
+ */
+
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/test-case-property-ordering');
+const RuleTester = require('eslint').RuleTester;
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run('test-case-property-ordering', rule, {
+  valid: [
+    `
+      new RuleTester().run('foo', bar, {
+        valid: [
+          {
+            code: "foo",
+            output: "bar",
+            options: ["baz"],
+          },
+        ]
+      });
+    `,
+    `
+      new RuleTester().run('foo', bar, {
+        valid: [
+          {
+            code: "foo",
+            output: "bar",
+            options: ["baz"],
+            env: { es6: true },
+          },
+        ]
+      });
+    `,
+    {
+      code: `
+        new RuleTester().run('foo', bar, {
+          valid: [
+            {
+              code: "foo",
+              options: ["baz"],
+              output: "bar",
+            },
+          ]
+        });
+      `,
+      options: [['code', 'errors', 'options', 'output', 'parserOptions']],
+    },
+  ],
+
+  invalid: [
+    {
+      code: `
+        new RuleTester().run('foo', bar, {
+          valid: [
+            {
+              code: "foo",
+              options: ["baz"],
+              output: "bar",
+            },
+          ]
+        });
+      `,
+      errors: [{ message: 'The properties of a test case should be placed in a consistent order: [code, output, options].' }],
+    },
+    {
+      code: `
+      new RuleTester().run('foo', bar, {
+        valid: [
+          {
+            env: { es6: true },
+            code: "foo",
+            output: "bar",
+            options: ["baz"],
+          },
+        ]
+      });
+      `,
+      errors: [{ message: 'The properties of a test case should be placed in a consistent order: [code, output, options, env].' }],
+    },
+    {
+      code: `
+        new RuleTester().run('foo', bar, {
+          valid: [
+            {
+              code: "foo",
+              env: { es6: true },
+              output: "bar",
+              options: ["baz"],
+            },
+          ]
+        });
+      `,
+      errors: [{ message: 'The properties of a test case should be placed in a consistent order: [code, output, options, env].' }],
+    },
+    {
+      code: `
+        new RuleTester().run('foo', bar, {
+          valid: [
+            {
+              code: "foo",
+              output: "bar",
+              options: ["baz"],
+            },
+          ]
+        });
+      `,
+      options: [['code', 'errors', 'options', 'output', 'parserOptions']],
+      errors: [{ message: 'The properties of a test case should be placed in a consistent order: [code, options, output].' }],
+    },
+    {
+      code: `
+        new RuleTester().run('foo', bar, {
+          valid: [
+            {
+              options: ["baz"],
+              parserOptions: "",
+              code: "foo",
+              errors: ["foo"],
+              output: "",
+            },
+          ]
+        });
+      `,
+      options: [['code', 'errors', 'output']],
+      errors: [{ message: 'The properties of a test case should be placed in a consistent order: [code, errors, output, options, parserOptions].' }],
+    },
+  ],
+});
