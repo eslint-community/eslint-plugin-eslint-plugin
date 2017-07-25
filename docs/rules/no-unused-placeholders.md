a090610265260d681dcf55f45b16252d957ec262
薛定谔的猫 <hh_2013@foxmail.com>
2017-07-25 21:22:19 +0800

    New: rule no-unused-placeholders. (#26)
    * New: rule no-unused-placeholders.
    
    * Docs: no-unused-placeholders.
    
    * Fix: accept review suggestions.

---
+++ docs/rules/no-unused-placeholders.md
@@ -0,0 +1,58 @@
+# Disallow unused placeholders in rule report messages (no-unused-placeholders)
+
+This rule aims to disallow unused placeholders in rule report messages.
+
+## Rule Details
+
+Reports when a context.report call contains a data property that does not have a corresponding placeholder in the report message.
+
+Examples of **incorrect** code for this rule:
+
+```js
+/*eslint eslint-plugin/no-missing-placeholders: error*/
+
+module.exports = {
+  create(context) {
+
+    context.report({
+      node,
+      message: 'something is wrong.',
+      data: { something: 'foo' }
+    });
+
+    context.report(node, 'something is wrong.', { something: 'foo' });
+  }
+};
+```
+
+Examples of **correct** code for this rule:
+
+```js
+/*eslint eslint-plugin/no-missing-placeholders: error*/
+
+module.exports = {
+  create(context) {
+    context.report({
+      node,
+      message: 'something is wrong.'
+    });
+
+    context.report({
+      node,
+      message: '{{something}} is wrong.',
+      data: { something: 'foo' }
+    });
+
+    context.report(node, '{{something}} is wrong.', { something: 'foo' });
+  }
+};
+```
+
+## When Not To Use It
+
+If you want to allow unused placeholders, you should turn off this rule.
+
+## Further Reading
+
+* [context.report() API](http://eslint.org/docs/developer-guide/working-with-rules#contextreport)
+* [no-missing-placeholders](https://github.com/not-an-aardvark/eslint-plugin-eslint-plugin/blob/master/docs/rules/no-missing-placeholders.md)
---
+++ tests/lib/rules/no-unused-placeholders.js
@@ -0,0 +1,142 @@
+/**
+ * @fileoverview Disallow unused placeholders in rule report messages
+ * @author 薛定谔的猫<hh_2013@foxmail.com>
+ */
+
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-unused-placeholders');
+const RuleTester = require('eslint').RuleTester;
+
+/**
+* Create an error for the given key
+* @param {string} unusedKey The placeholder that is unused
+* @returns {object} An expected error
+*/
+function error (unusedKey) {
+  return { type: 'Literal', message: `The placeholder {{${unusedKey}}} is unused.` };
+}
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+ruleTester.run('no-unused-placeholders', rule, {
+
+  valid: [
+    `
+          module.exports = {
+            create(context) {
+              context.report({
+                node,
+                message: 'foo{{ bar }}',
+                data: { bar: 'baz' }
+              });
+            }
+          };
+        `,
+    `
+        module.exports = {
+        create(context) {
+          context.report({
+            node,
+            message: 'foo bar'
+          });
+        }
+      };
+    `,
+    `
+      module.exports = {
+        create(context) {
+          context.report({
+            node,
+            message: 'foo {{bar}}',
+            data: { bar: 'baz' }
+          });
+        }
+      };
+    `,
+    `
+      module.exports = {
+        create(context) {
+          context.report({
+            node,
+            message: 'foo {{bar}}',
+            data: { 'bar': 'baz' }
+          });
+        }
+      };
+    `,
+    `
+      module.exports = {
+        create(context) {
+          context.report({
+            node,
+            message: 'foo {{bar}}',
+            data: { bar }
+          });
+        }
+      };
+    `,
+    `
+      module.exports = context => {
+        context.report(node, 'foo {{bar}}', { bar: 'baz' });
+      };
+    `,
+    `
+      module.exports = context => {
+        context.report(node, { line: 1, column: 3 }, 'foo {{bar}}', { bar: 'baz' });
+      };
+    `,
+  ],
+
+  invalid: [
+    {
+      code: `
+        module.exports = {
+          create(context) {
+            context.report({
+              node,
+              message: 'foo',
+              data: { bar }
+            });
+          }
+        };
+      `,
+      errors: [error('bar')],
+    },
+    {
+      code: `
+        module.exports = {
+          create(context) {
+            context.report({
+              node,
+              message: 'foo',
+              data: { bar: '' }
+            });
+          }
+        };
+      `,
+      errors: [error('bar')],
+    },
+    {
+      code: `
+        module.exports = {
+          create(context) {
+            context.report({
+              node,
+              message: 'foo {{bar}}',
+              data: { baz: '' }
+            });
+          }
+        };
+      `,
+      errors: [error('baz')],
+    },
+  ],
+});
