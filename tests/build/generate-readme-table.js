843f3eef69eb264fa46d2cd42d5de602d3f378b5
Teddy Katz <teddy.katz@gmail.com>
2017-09-10 01:33:19 -0400

    Build: autogenerate the table in README.md (fixes #43)

---
+++ tests/build/generate-readme-table.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const assert = require('chai').assert;
+
+describe('table in README.md', () => {
+  it('is up-to-date', () => {
+    const actualReadme = fs.readFileSync(path.resolve(__dirname, '..', '..', 'README.md'), 'utf8');
+    const expectedReadme = require('../../build/generate-readme-table');
+
+    assert(
+      actualReadme === expectedReadme,
+      'The table in README.md is out of date. Please use `npm run generate-readme-table` to update it.'
+    );
+  });
+});
