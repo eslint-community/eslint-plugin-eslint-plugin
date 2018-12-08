7f8794159aae178fdd6f069ed9d4dee27367633a
薛定谔的猫 <weiran.zsd@outlook.com>
2018-12-08 13:47:32 +0800

    New: require-meta-type (fixes #67) (#68)
    refs:
    * https://eslint.org/docs/user-guide/command-line-interface#--fix-type
    * https://eslint.org/docs/developer-guide/working-with-rules#rule-basics
    * https://eslint.org/blog/2018/11/eslint-v5.9.0-released#the-fix-type-option

---
+++ tests/lib/rules/require-meta-type.js
@@ -0,0 +1,62 @@
+/**
+ * @fileoverview require rules to implement a meta.type property
+ * @author 唯然<weiran.zsd@outlook.com>
+ */
+
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/require-meta-type');
+const RuleTester = require('eslint').RuleTester;
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+ruleTester.run('require-meta-type', rule, {
+  valid: [
+    `
+      module.exports = {
+        meta: { type: 'problem' },
+        create(context) {}
+      };
+    `,
+    `
+      module.exports = {
+        meta: { type: 'suggestion' },
+        create(context) {}
+      };
+    `,
+    `
+      module.exports = {
+        meta: { type: 'layout' },
+        create(context) {}
+      };
+    `,
+  ],
+
+  invalid: [
+    {
+      code: `
+        module.exports = {
+          meta: {},
+          create(context) {}
+        };
+      `,
+      errors: [{ messageId: 'missing' }],
+    },
+    {
+      code: `
+        module.exports = {
+          meta: { type: 'invalid-type' },
+          create(context) {}
+        };
+      `,
+      errors: [{ messageId: 'unexpected' }],
+    },
+  ],
+});
