62716a4143100d4a07c64247bc60ccde9aa50d7b
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-06-12 11:32:33 -0700

    Add hasSuggestions property to meta-property-ordering rule
    It makes most sense to order the new ESLint 8 property `meta.hasSuggestions` after the related property `meta.fixable`.
    
    https://eslint.org/blog/2021/06/whats-coming-in-eslint-8.0.0#rules-with-suggestions-now-require-the-metahassuggestions-property

--- tests/lib/rules/meta-property-ordering.js
+++ tests/lib/rules/meta-property-ordering.js
@@ -42,6 +42,7 @@ ruleTester.run('test-case-property-ordering', rule, {
         type: 'problem',
         docs: {},
         fixable: 'code',
+        hasSuggestions: true,
         schema: [],
         messages: {}
       },
