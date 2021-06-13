ff0ae38e95d79ff316d77dbc5fd41b7d27c45bb0
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-06-13 01:39:13 -0700

    New: add new rule require-meta-has-suggestions (#105)
    Suggestable ESLint rules should have a `meta.hasSuggestions` property to indicate that they provide [suggestions](https://eslint.org/docs/developer-guide/working-with-rules#providing-suggestions). This new rule enforces that the `meta.hasSuggestions` property is correctly enabled when a rule provides suggestions, and not enabled when a rule does not provide suggestions.
    
    The change to require suggestable rules to have `meta.hasSuggestions` has been accepted and mentioned in the blog post for the upcoming ESLint 8 breaking changes. So we should adopt this change now to help plugin authors ensure they are compatible with ESLint 8 as soon as possible. The old property `meta.docs.suggestion` was unused anyway.
    
    https://eslint.org/blog/2021/06/whats-coming-in-eslint-8.0.0#rules-with-suggestions-now-require-the-metahassuggestions-property
    
    This is very similar to the existing [eslint-plugin/require-meta-fixable](https://github.com/not-an-aardvark/eslint-plugin-eslint-plugin/blob/master/docs/rules/require-meta-fixable.md) rule enforcing the correct presence of the `meta.fixable` property.

---
+++ tests/lib/rules/require-meta-has-suggestions.js
@@ -0,0 +1,194 @@
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/require-meta-has-suggestions');
+const RuleTester = require('eslint').RuleTester;
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+ruleTester.run('require-meta-has-suggestions', rule, {
+  valid: [
+    'module.exports = context => {};',
+    // No suggestions reported, no violations reported, no meta object.
+    `
+      module.exports = {
+        create(context) {}
+      };
+    `,
+    // No suggestions reported, no violations reported, empty meta object.
+    `
+      module.exports = {
+        meta: {},
+        create(context) {}
+      };
+    `,
+    // No suggestions reported, violation reported, empty meta object.
+    `
+      module.exports = {
+        meta: {},
+        create(context) {
+          context.report({node, message});
+        }
+      };
+    `,
+    // No suggestions reported, no suggestion property, non-object style of reporting.
+    `
+    module.exports = {
+      meta: {},
+      create(context) {
+        context.report(node, message);
+      }
+    };
+    `,
+    // No suggestions reported (empty suggest array), no suggestion property.
+    `
+    module.exports = {
+      meta: {},
+      create(context) {
+        context.report({node, message, suggest:[]});
+      }
+    };
+    `,
+    // No suggestions reported (empty suggest array in variable), no suggestion property.
+    `
+    const SUGGESTIONS = [];
+    module.exports = {
+      meta: {},
+      create(context) {
+        context.report({node, message, suggest: SUGGESTIONS});
+      }
+    };
+    `,
+    // No suggestions reported, hasSuggestions property set to false.
+    `
+    module.exports = {
+      meta: { hasSuggestions: false },
+      create(context) {
+        context.report({node, message});
+      }
+    };
+    `,
+    // No suggestions reported, hasSuggestions property set to false (as variable).
+    `
+    const hasSuggestions = false;
+    module.exports = {
+      meta: { hasSuggestions },
+      create(context) {
+        context.report({node, message});
+      }
+    };
+    `,
+    // Provides suggestions, has hasSuggestions property.
+    `
+      module.exports = {
+        meta: { hasSuggestions: true },
+        create(context) {
+          context.report({node, message, suggest: [{}]});
+        }
+      };
+    `,
+    // Provides suggestions, has hasSuggestions property (as variable).
+    `
+      const hasSuggestions = true;
+      module.exports = {
+        meta: { hasSuggestions },
+        create(context) {
+          context.report({node, message, suggest: [{}]});
+        }
+      };
+    `,
+    // Provides *dynamic* suggestions, has hasSuggestions property.
+    `
+      module.exports = {
+        meta: { hasSuggestions: true },
+        create(context) {
+          context.report({node, message, suggest: getSuggestions()});
+        }
+      };
+    `,
+    // Spread syntax.
+    {
+      code: `
+        const meta = {};
+        module.exports = {
+          ...meta,
+          meta: {},
+          create(context) { context.report(node, message, data, fix); }
+        };
+      `,
+      parserOptions: {
+        ecmaVersion: 9,
+      },
+    },
+  ],
+
+  invalid: [
+    {
+      // Reports suggestions, no meta object, violation should be on `create` function.
+      code: `
+        module.exports = {
+          create(context) { context.report({node, message, suggest: [{}]}); }
+        };
+      `,
+      errors: [{ messageId: 'shouldBeSuggestable', type: 'FunctionExpression', line: 3, column: 17, endLine: 3, endColumn: 78 }],
+    },
+    {
+      // Reports suggestions, no hasSuggestions property, violation should be on `meta` object.
+      code: `
+        module.exports = {
+          meta: {},
+          create(context) { context.report({node, message, suggest: [{}]}); }
+        };
+      `,
+      errors: [{ messageId: 'shouldBeSuggestable', type: 'ObjectExpression', line: 3, column: 17, endLine: 3, endColumn: 19 }],
+    },
+    {
+      // Reports suggestions (in variable), no hasSuggestions property, violation should be on `meta` object.
+      code: `
+        const SUGGESTIONS = [{}];
+        module.exports = {
+          meta: {},
+          create(context) { context.report({node, message, suggest: SUGGESTIONS}); }
+        };
+      `,
+      errors: [{ messageId: 'shouldBeSuggestable', type: 'ObjectExpression', line: 4, column: 17, endLine: 4, endColumn: 19 }],
+    },
+    {
+      // Reports suggestions, hasSuggestions property set to false, violation should be on `false`
+      code: `
+        module.exports = {
+          meta: { hasSuggestions: false },
+          create(context) { context.report({node, message, suggest: [{}]}); }
+        };
+      `,
+      errors: [{ messageId: 'shouldBeSuggestable', type: 'Literal', line: 3, column: 35, endLine: 3, endColumn: 40 }],
+    },
+    {
+      // Reports suggestions, hasSuggestions property set to false (as variable), violation should be on variable
+      code: `
+        const hasSuggestions = false;
+        module.exports = {
+          meta: { hasSuggestions },
+          create(context) { context.report({node, message, suggest: [{}]}); }
+        };
+      `,
+      errors: [{ messageId: 'shouldBeSuggestable', type: 'Identifier', line: 4, column: 19, endLine: 4, endColumn: 33 }],
+    },
+    {
+      // Does not report suggestions, hasSuggestions property set to true, violation should be on `true`
+      code: `
+        module.exports = {
+          meta: { hasSuggestions: true },
+          create(context) { context.report({node, message}); }
+        };
+      `,
+      errors: [{ messageId: 'shouldNotBeSuggestable', type: 'Literal', line: 3, column: 35, endLine: 3, endColumn: 39 }],
+    },
+  ],
+});
