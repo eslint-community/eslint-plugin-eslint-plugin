13222d9db1117caa42c96a591c4487bcd7a40f49
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-06-14 12:47:25 -0700

    refactor: switch from .eslintrc.yml to .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  plugins: ['node', 'self'],
+  extends: [
+    'not-an-aardvark/node',
+    'plugin:node/recommended',
+    'plugin:self/all',
+  ],
+  rules: {
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        functions: 'never', // disallow trailing commas in function(es2017)
+      },
+    ],
+    'require-jsdoc': 'error',
+
+    'self/meta-property-ordering': 'off',
+    'self/require-meta-docs-url': 'off',
+    'self/report-message-format': ['error', '^[^a-z].*.$'],
+  },
+};
