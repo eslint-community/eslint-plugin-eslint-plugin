5fbb15b831225a1564c0e904caa5d13b621a448c
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-06-16 08:15:16 -0700

    chore(dev-deps): add eslint-plugin-unicorn
    https://github.com/sindresorhus/eslint-plugin-unicorn

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,12 @@ module.exports = {
     'not-an-aardvark/node',
     'plugin:node/recommended',
     'plugin:self/all',
+    'plugin:unicorn/recommended',
   ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'script',
+  },
   rules: {
     'comma-dangle': [
       'error',
@@ -22,6 +27,14 @@ module.exports = {
     'self/meta-property-ordering': 'off',
     'self/require-meta-docs-url': 'off',
     'self/report-message-format': ['error', '^[^a-z].*.$'],
+
+    'unicorn/consistent-function-scoping': 'off',
+    'unicorn/no-array-callback-reference': 'off',
+    'unicorn/no-array-for-each': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/prefer-module': 'off',
+    'unicorn/prevent-abbreviations': 'off',
   },
   overrides: [
     {
