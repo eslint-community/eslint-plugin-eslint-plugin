0bfea942163f297df208de3e9d44250918ed044e
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-07-09 11:18:56 -0400

    Fix: Fix false positives/negatives in `require-meta-fixable` rule
    False positives fixed:
    
    * When `meta.fixable` uses a variable
    
    False negatives fixed:
    
    * When there's a fixer but `meta.fixable` = null
    * When there's a fixer but `meta.fixable` = undefined
    
    Also improves reporting location.

--- tests/lib/rules/require-meta-fixable.js
+++ tests/lib/rules/require-meta-fixable.js
@@ -12,9 +12,6 @@
 const rule = require('../../../lib/rules/require-meta-fixable');
 const RuleTester = require('eslint').RuleTester;
 
-const MISSING_ERROR = { message: 'Fixable rules must export a `meta.fixable` property.', type: 'FunctionExpression' };
-const INVALID_ERROR = { message: '`meta.fixable` must be either `code`, `whitespace` or `null`.', type: 'Property' };
-
 // ------------------------------------------------------------------------------
 // Tests
 // ------------------------------------------------------------------------------
@@ -32,6 +29,16 @@ ruleTester.run('require-meta-fixable', rule, {
     `
       module.exports = {
         meta: { fixable: 'code' },
+        create(context) {
+          context.report({node, message, fix: foo});
+        }
+      };
+    `,
+    // Value in variable.
+    `
+      const fixable = 'code';
+      module.exports = {
+        meta: { fixable },
         create(context) {
           context.report({node, message, fix: foo});
         }
@@ -93,6 +100,13 @@ ruleTester.run('require-meta-fixable', rule, {
         }
       };
     `,
+    // `fixable` uses variable but no static value available.
+    `
+      module.exports = {
+        meta: { fixable: foo },
+        create(context) { context.report({node, message, fix: foo}); }
+      };
+    `,
     `
       module.exports = {
         meta: {},
@@ -128,7 +142,15 @@ ruleTester.run('require-meta-fixable', rule, {
           create(context) { context.report({node, message, fix: foo}); }
         };
       `,
-      errors: [MISSING_ERROR],
+      errors: [{ messageId: 'missing', type: 'ObjectExpression' }],
+    },
+    {
+      code: `
+        module.exports = {
+          create(context) { context.report({node, message, fix: foo}); }
+        };
+      `,
+      errors: [{ messageId: 'missing', type: 'FunctionExpression' }],
     },
     {
       code: `
@@ -137,7 +159,7 @@ ruleTester.run('require-meta-fixable', rule, {
           create(context) { context.report(node, loc, message, data, fix); }
         };
       `,
-      errors: [MISSING_ERROR],
+      errors: [{ messageId: 'missing', type: 'ObjectExpression' }],
     },
     {
       code: `
@@ -146,7 +168,7 @@ ruleTester.run('require-meta-fixable', rule, {
           create(context) { context.report({node, message}); }
         };
       `,
-      errors: [INVALID_ERROR],
+      errors: [{ messageId: 'invalid', type: 'Literal' }],
     },
     {
       code: `
@@ -155,16 +177,35 @@ ruleTester.run('require-meta-fixable', rule, {
           create(context) { context.report({node, message, fix: foo}); }
         };
       `,
-      errors: [INVALID_ERROR],
+      errors: [{ messageId: 'invalid', type: 'Literal' }],
+    },
+    {
+      code: `
+        const fixable = 'invalid';
+        module.exports = {
+          meta: { fixable },
+          create(context) { context.report({node, message, fix: foo}); }
+        };
+      `,
+      errors: [{ messageId: 'invalid', type: 'Identifier' }],
     },
     {
       code: `
         module.exports = {
-          meta: { fixable: foo },
+          meta: { fixable: null },
           create(context) { context.report({node, message, fix: foo}); }
         };
       `,
-      errors: [INVALID_ERROR],
+      errors: [{ messageId: 'missing', type: 'Literal' }],
+    },
+    {
+      code: `
+        module.exports = {
+          meta: { fixable: undefined },
+          create(context) { context.report({node, message, fix: foo}); }
+        };
+      `,
+      errors: [{ messageId: 'missing', type: 'Identifier' }],
     },
   ],
 });
