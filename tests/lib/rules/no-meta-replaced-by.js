4d3a319b96b95a731c51f5d2eb89260e4630da41
error-four-o-four <jens.bachmayer@gmx.de>
2025-06-17 20:34:00 +0200

    test: add `no-meta-replaced-by` tests

---
+++ tests/lib/rules/no-meta-replaced-by.js
@@ -0,0 +1,132 @@
+/**
+ * @fileoverview require rules to implement a `meta.type` property
+ * @author 唯然<weiran.zsd@outlook.com>
+ */
+
+'use strict';
+
+// ------------------------------------------------------------------------------
+// Requirements
+// ------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-meta-replaced-by');
+const RuleTester = require('../eslint-rule-tester').RuleTester;
+
+// ------------------------------------------------------------------------------
+// Tests
+// ------------------------------------------------------------------------------
+
+const valid = [
+  'module.exports = {};',
+  `
+    module.exports = {
+      create(context) {},
+    };
+  `,
+  `
+    module.exports = {
+      meta: {},
+      create(context) {},
+    };
+  `,
+  {
+    code: `
+      module.exports = {
+        meta: {
+          deprecated: {
+            replacedBy: [
+              {
+                rule: {
+                  name: 'foo',
+                },
+              },
+            ],
+          },
+        },
+        create(context) {},
+      };
+    `,
+    errors: 0,
+  },
+];
+
+const invalid = [
+  {
+    code: `
+      module.exports = {
+        meta: {
+          replacedBy: [],
+        },
+        create(context) {},
+      };
+    `,
+    errors: [
+      {
+        messageId: 'useNewFormat',
+        line: 4,
+        endLine: 4,
+      },
+    ],
+  },
+  {
+    code: `
+      const meta = {
+        replacedBy: null,
+      };
+
+      module.exports = {
+        meta,
+        create(context) {},
+      };
+    `,
+    errors: [
+      {
+        messageId: 'useNewFormat',
+        line: 3,
+        endLine: 3,
+      },
+    ],
+  },
+  {
+    code: `
+      const spread = {
+        replacedBy: null,
+      };
+
+      module.exports = {
+        meta: {
+          ...spread,
+        },
+        create(context) {},
+      };
+    `,
+    errors: [{ messageId: 'useNewFormat' }],
+  },
+];
+
+const testToESM = (test) => {
+  if (typeof test === 'string') {
+    return test.replace('module.exports =', 'export default');
+  }
+
+  const code = test.code.replace('module.exports =', 'export default');
+
+  return {
+    ...test,
+    code,
+  };
+};
+
+new RuleTester({
+  languageOptions: { sourceType: 'commonjs' },
+}).run('no-meta-replaced-by', rule, {
+  valid,
+  invalid,
+});
+
+new RuleTester({
+  languageOptions: { sourceType: 'module' },
+}).run('no-meta-replaced-by', rule, {
+  valid: valid.map(testToESM),
+  invalid: invalid.map(testToESM),
+});
