bdc07e063afbdf4fe8bb39f774c9e172eafa8e96
michael faith <michaelfaith@users.noreply.github.com>
2025-08-08 17:03:34 -0500

    remove tsdown defaults

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'tsdown';
 
 export default defineConfig({
   unbundle: true,
-  clean: true,
   dts: true,
   entry: ['lib/**/*.ts'],
-  format: ['esm'],
   outDir: 'dist',
 });
