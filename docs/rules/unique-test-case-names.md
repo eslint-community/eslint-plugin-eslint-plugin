292a371bac72abd93eefa28fb8fa3a1ba881a4a0
michael faith <michaelfaith@users.noreply.github.com>
2025-10-25 07:45:53 -0500

    feat: add rule unique-test-case-names (#561)
    This change adds a new rule to require that any test cases that have names defined, have unique names within each `valid` and `invalid` group.  This helps ensure that test logs are unambiguous.

---
+++ docs/rules/unique-test-case-names.md
@@ -0,0 +1,73 @@
+# Enforce that all test cases with names have unique names (`eslint-plugin/unique-test-case-names`)
+
+<!-- end auto-generated rule header -->
+
+This rule enforces that any test cases that have names defined, have unique names within their `valid` and `invalid` arrays.
+
+## Rule Details
+
+This rule aims to ensure test suites are producing logs in a form that make it easy to identify failing tests, when they occur.
+For thoroughly tested rules, it's not uncommon for test cases to have names defined so that they're easily distinguishable in the test log output.
+Requiring that, within each `valid` and `invalid` group, any test cases with names have unique names, it ensures the test logs are unambiguous.
+
+Examples of **incorrect** code for this rule:
+
+```js
+new RuleTester().run('foo', bar, {
+  valid: [
+    {
+      code: 'nin',
+      name: 'test case 1',
+    },
+    {
+      code: 'smz',
+      name: 'test case 1',
+    },
+  ],
+  invalid: [
+    {
+      code: 'foo',
+      errors: ['Error'],
+      name: 'test case 2',
+    },
+    {
+      code: 'bar',
+      errors: ['Error'],
+      name: 'test case 2',
+    },
+  ],
+});
+```
+
+Examples of **correct** code for this rule:
+
+```js
+new RuleTester().run('foo', bar, {
+  valid: [
+    {
+      code: 'nin',
+      name: 'test case 1',
+    },
+    {
+      code: 'smz',
+      name: 'test case 2',
+    },
+  ],
+  invalid: [
+    {
+      code: 'foo',
+      errors: ['Error'],
+      name: 'test case 1',
+    },
+    {
+      code: 'bar',
+      errors: ['Error'],
+      name: 'test case 2',
+    },
+  ],
+});
+```
+
+## When Not to Use It
+
+If you aren't concerned with the nature of test logs.
---
+++ lib/rules/unique-test-case-names.ts
@@ -0,0 +1,84 @@
+import type { Rule } from 'eslint';
+
+import { evaluateObjectProperties, getKeyName, getTestInfo } from '../utils.ts';
+import type { TestInfo } from '../types.ts';
+
+const rule: Rule.RuleModule = {
+  meta: {
+    type: 'suggestion',
+    docs: {
+      description: 'enforce that all test cases with names have unique names',
+      category: 'Tests',
+      recommended: false,
+      url: 'https://github.com/eslint-community/eslint-plugin-eslint-plugin/tree/HEAD/docs/rules/unique-test-case-names.md',
+    },
+    schema: [],
+    messages: {
+      notUnique:
+        'This test case name is not unique.  All test cases with names should have unique names.',
+    },
+  },
+
+  create(context) {
+    const sourceCode = context.sourceCode;
+
+    /**
+     * Validates test cases and reports them if found in violation
+     * @param cases A list of test case nodes
+     */
+    function validateTestCases(cases: TestInfo['valid']): void {
+      // Gather all of the information from each test case
+      const namesSeen = new Set<string>();
+      const violatingNodes: NonNullable<TestInfo['valid'][number]>[] = [];
+
+      cases
+        .filter((testCase) => !!testCase)
+        .forEach((testCase) => {
+          if (testCase.type === 'ObjectExpression') {
+            for (const property of evaluateObjectProperties(
+              testCase,
+              sourceCode.scopeManager,
+            )) {
+              if (property.type === 'Property') {
+                const keyName = getKeyName(
+                  property,
+                  sourceCode.getScope(testCase),
+                );
+                if (
+                  keyName === 'name' &&
+                  property.value.type === 'Literal' &&
+                  typeof property.value.value === 'string'
+                ) {
+                  const name = property.value.value;
+                  if (namesSeen.has(name)) {
+                    violatingNodes.push(property.value);
+                  } else {
+                    namesSeen.add(name);
+                  }
+                  break;
+                }
+              }
+            }
+          }
+        });
+
+      for (const node of violatingNodes) {
+        context.report({
+          node,
+          messageId: 'notUnique',
+        });
+      }
+    }
+
+    return {
+      Program(ast) {
+        getTestInfo(context, ast).forEach((testInfo) => {
+          validateTestCases(testInfo.valid);
+          validateTestCases(testInfo.invalid);
+        });
+      },
+    };
+  },
+};
+
+export default rule;
