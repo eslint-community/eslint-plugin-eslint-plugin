29420d6fbd93aa0d8812691b12073d05c37373cc
唯然 <weiran.zsd@outlook.com>
2026-01-31 22:32:02 +0800

    chore: update eslint-plugin-eslint-comments to v4.6.0 and eslint-plugin-unicorn to v62.0.0

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,12 +1,14 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 import prettier from 'eslint-config-prettier/flat';
 import markdown from 'eslint-plugin-markdown';
-import pluginN from 'eslint-plugin-n';
+import n from 'eslint-plugin-n';
 import tseslint from 'typescript-eslint';
-
+import unicorn from 'eslint-plugin-unicorn';
+import eslintComments from '@eslint-community/eslint-plugin-eslint-comments/configs'
 import eslintPlugin from './lib/index.ts';
 
 const dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -15,7 +17,7 @@ const compat = new FlatCompat({
   recommendedConfig: js.configs.recommended,
 });
 
-export default tseslint.config([
+export default defineConfig([
   // Global ignores
   {
     ignores: [
@@ -26,19 +28,19 @@ export default tseslint.config([
       'e2e/fixtures',
     ],
   },
-  // Global settings
-  {
-    languageOptions: { parser: tseslint.parser, sourceType: 'module' },
-  },
   ...compat.extends(
     'not-an-aardvark/node',
-    'plugin:@eslint-community/eslint-comments/recommended',
-    'plugin:unicorn/recommended',
   ),
-  pluginN.configs['flat/recommended'],
-  prettier,
+
+  // base config
   {
+    files: ['**/*.{js,ts}'],
+    languageOptions: { parser: tseslint.parser, sourceType: 'module' },
+    plugins: { js, n, 'eslint-plugin': eslintPlugin },
+    extends: [prettier, "js/recommended", "n/recommended", unicorn.configs.recommended, eslintComments.recommended],
     rules: {
+      'n/no-missing-import': 'off',
+
       '@eslint-community/eslint-comments/no-unused-disable': 'error',
       '@eslint-community/eslint-comments/require-description': 'error',
 
@@ -52,18 +54,13 @@ export default tseslint.config([
       'unicorn/no-nested-ternary': 'off',
     },
   },
-  // TypeScript rules
-  tseslint.configs.recommended.map((config) => ({
-    files: ['**/*.ts', '**/*.mts', '**/*.cts'],
-    ...config,
-    rules: { ...config.rules, 'n/no-missing-import': 'off' },
-  })),
+
   {
     // Apply eslint-plugin rules to our own rules/tests (but not docs).
     files: ['lib/**/*.ts', 'tests/**/*.ts'],
     plugins: { 'eslint-plugin': eslintPlugin },
+    extends: ['eslint-plugin/all'],
     rules: {
-      ...eslintPlugin.configs.all.rules,
       'eslint-plugin/no-meta-schema-default': 'off', // TODO: enable once https://github.com/bmish/eslint-doc-generator/issues/513 is fixed and released
       'eslint-plugin/report-message-format': ['error', '^[^a-z].*.$'],
       'eslint-plugin/require-meta-docs-url': [
